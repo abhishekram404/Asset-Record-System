9864b7ea688f7ba36a0ca9201382d216eda06d49
Sagar Pariyar <sagarpariyar345@gmail.com>
2024-01-25 21:38:21 -0500

    Login Page added

---
+++ Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        PictureBox1 = New PictureBox()
+        Label2 = New Label()
+        Panel1 = New Panel()
+        txtUsername = New TextBox()
+        PictureBox2 = New PictureBox()
+        Panel2 = New Panel()
+        txtPassword = New TextBox()
+        PictureBox3 = New PictureBox()
+        btnLogin = New Button()
+        btnCancel = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(156, 38)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(156, 156)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(195, 215)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 28)
+        Label2.TabIndex = 2
+        Label2.Text = "Sign In"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.White
+        Panel1.Controls.Add(txtUsername)
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Location = New Point(77, 268)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(323, 58)
+        Panel1.TabIndex = 3
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.BorderStyle = BorderStyle.None
+        txtUsername.Font = New Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtUsername.ForeColor = Color.Black
+        txtUsername.Location = New Point(64, 16)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(244, 23)
+        txtUsername.TabIndex = 1
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(9, 6)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(44, 44)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 0
+        PictureBox2.TabStop = False
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.White
+        Panel2.Controls.Add(txtPassword)
+        Panel2.Controls.Add(PictureBox3)
+        Panel2.Location = New Point(77, 332)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(323, 58)
+        Panel2.TabIndex = 4
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.BorderStyle = BorderStyle.None
+        txtPassword.Font = New Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtPassword.ForeColor = Color.Black
+        txtPassword.Location = New Point(64, 16)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(244, 23)
+        txtPassword.TabIndex = 1
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(9, 6)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(44, 44)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 0
+        PictureBox3.TabStop = False
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.BackColor = Color.FromArgb(CByte(0), CByte(120), CByte(212))
+        btnLogin.ForeColor = Color.White
+        btnLogin.Location = New Point(77, 441)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(323, 56)
+        btnLogin.TabIndex = 5
+        btnLogin.Text = "Login"
+        btnLogin.UseVisualStyleBackColor = False
+        ' 
+        ' btnCancel
+        ' 
+        btnCancel.Location = New Point(77, 503)
+        btnCancel.Name = "btnCancel"
+        btnCancel.Size = New Size(323, 56)
+        btnCancel.TabIndex = 6
+        btnCancel.Text = "Cancel"
+        btnCancel.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(240), CByte(244), CByte(247))
+        ClientSize = New Size(489, 648)
+        Controls.Add(btnCancel)
+        Controls.Add(btnLogin)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Controls.Add(Label2)
+        Controls.Add(PictureBox1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "LogIn"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnCancel As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtUsername.Text = "username"
+        txtUsername.ForeColor = Color.DarkGray
+
+        txtPassword.Text = "password"
+        txtPassword.ForeColor = Color.DarkGray
+    End Sub
+
+    Private Sub txtUsername_GotFocus(sender As Object, e As EventArgs) Handles txtUsername.GotFocus
+        If txtUsername.Text = "username" Then
+            txtUsername.Text = ""
+            txtUsername.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub txtUsername_LostFocus(sender As Object, e As EventArgs) Handles txtUsername.LostFocus
+        If txtUsername.Text = "" Then
+            txtUsername.Text = "username"
+            txtUsername.ForeColor = Color.DarkGray
+        End If
+    End Sub
+
+    Private Sub txtPassword_GotFocus(sender As Object, e As EventArgs) Handles txtPassword.GotFocus
+        If txtPassword.Text = "password" Then
+            txtPassword.Text = ""
+            txtPassword.PasswordChar = "•"
+            txtPassword.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub txtPassword_LostFocus(sender As Object, e As EventArgs) Handles txtPassword.LostFocus
+        If txtPassword.Text = "" Then
+            txtPassword.Text = "password"
+            txtPassword.PasswordChar = ""
+            txtPassword.ForeColor = Color.DarkGray
+        End If
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Me.Hide()
+
+    End Sub
+End Class
