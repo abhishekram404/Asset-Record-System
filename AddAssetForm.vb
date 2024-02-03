0a2045a78bfd7bc66f0017b19071c066f683f013
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-02-04 01:03:49 +0545

    Add login functionality

--- AddAssetForm.vb
+++ AddAssetForm.vb
@@ -1,4 +1,10 @@
-﻿Public Class AddAssetForm
+﻿Imports System.Data.SqlClient
+
+Public Class AddAssetForm
+
+    Private Sub SubmitNewAssetButton_Click(sender As Object, e As EventArgs) Handles SubmitNewAssetButton.Click
+
+    End Sub
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
--- LoginForm.vb
+++ LoginForm.vb
@@ -8,6 +8,30 @@
     End Sub
 
     Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        Dim username As String = UsernameField.Text
+        Dim password As String = PasswordField.Text
+
+        Dim query As String = $"SELECT top 1 * from Admins where username='{username}'"
+
+        Dim dataTable As DataTable = DatabaseHelper.ExecuteQuery(query)
+
+        ' If no users found then return after showing a message
+        If dataTable.Rows.Count = 0 Then
+            MessageBox.Show("No admin found with given username!", "Invalid credentials", MessageBoxButtons.OK)
+            Return
+        End If
+
+        Dim storedHashPassword = dataTable.Rows(0)("passwordHash").ToString()
+
+        Dim isPasswordCorrect As Boolean = PasswordHasher.VerifyPassword(password, storedHashPassword)
+
+        ' If password is not correct then return
+        If Not isPasswordCorrect Then
+            MessageBox.Show("Password is incorrect", "Error")
+            Return
+        End If
+
+        ' If execution reached here then the credentials are valid, and the user is authenticated
         Me.Hide()
         Dashboard.Show()
     End Sub
